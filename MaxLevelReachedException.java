80dda5119f2fa5d6bf9eb3432060ede16ae44e40
pats721 <pats72194@yahoo.com>
2017-03-28 17:02:58 -0400

    MaxLevelReachedException 1.0
    put in data package

---
+++ MaxLevelReachedException.java
@@ -0,0 +1,11 @@
+package data;
+
+public class MaxLevelReachedException extends Exception {
+	
+	public MaxLevelReachedException(){}
+	
+	public MaxLevelReachedException(String message) {
+		super(message);
+	}
+
+}
